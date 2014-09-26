a437a479c975ad6baaa51c9d7a98757bfbec3a29
Tom Craven <tcraven@amazon.com>
2014-09-26 09:20:46 +0100

    spn/draw: Colour - add colour namespace with conversion functions from RGBA to int and vice versa

--- spn/include/draw/Colour.h
+++ spn/include/draw/Colour.h
@@ -3,7 +3,29 @@
 
 namespace draw
 {
-	const uint32_t kLightBlue = 0xF8DEAF;
+	namespace colour
+	{
+		const uint32_t kDefault = 0xFFFFFFFF;
+
+		const uint32_t kLightBlue = 0xFFF8DEAF;
+		
+		uint32_t colourFromRGBA( uint8_t r, uint8_t g, uint8_t b, uint8_t a = 0xFF )
+		{
+			return ( ( uint32_t )r ) |
+                ( ( uint32_t )g << 8 ) |
+                ( ( uint32_t )b << 16) |
+                ( ( uint32_t )a << 24 );
+		}
+
+		void rgbaFromColour( uint32_t colour, uint8_t& r, uint8_t& g, uint8_t& b, uint8_t& a )
+		{
+			// untested
+			r = colour & 0xFF;
+			g = ( colour >> 8 ) & 0xFF;
+			b = ( colour >> 16 ) & 0xFF;
+			a = ( colour >> 24 ) & 0xFF;
+		}
+	}
 }
 
 #endif
