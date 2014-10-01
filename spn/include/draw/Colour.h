429e86b3520349877a0d328bdaf6da5084ab0e6c
Tom Craven <tcraven@amazon.com>
2014-10-01 08:45:20 +0100

    spn/draw: Colour - add 'inline static' to colourFromRGBA and rgbaFromColour functions to avoid linker warning

--- spn/include/draw/Colour.h
+++ spn/include/draw/Colour.h
@@ -6,10 +6,9 @@ namespace draw
 	namespace colour
 	{
 		const uint32_t kDefault = 0xFFFFFFFF;
-
 		const uint32_t kLightBlue = 0xFFF8DEAF;
-		
-		uint32_t colourFromRGBA( uint8_t r, uint8_t g, uint8_t b, uint8_t a = 0xFF )
+
+		inline static uint32_t colourFromRGBA( uint8_t r, uint8_t g, uint8_t b, uint8_t a = 0xFF )
 		{
 			return ( ( uint32_t )r ) |
                 ( ( uint32_t )g << 8 ) |
@@ -17,14 +16,14 @@ namespace draw
                 ( ( uint32_t )a << 24 );
 		}
 
-		void rgbaFromColour( uint32_t colour, uint8_t& r, uint8_t& g, uint8_t& b, uint8_t& a )
+		inline static void rgbaFromColour( uint32_t colour, uint8_t& r, uint8_t& g, uint8_t& b, uint8_t& a )
 		{
 			r = colour & 0xFF;
 			g = ( colour >> 8 ) & 0xFF;
 			b = ( colour >> 16 ) & 0xFF;
 			a = ( colour >> 24 ) & 0xFF;
 		}
-	}
+	};
 }
 
 #endif
