af03ae69bf152e944b8770fecfadc08fcb496faa
Tom Craven <tcraven@amazon.com>
2014-10-14 08:40:17 +0100

    core: Assert - add VALIDATE & VALIDATE_AND_RETURN macros

--- spn/include/core/Assert.h
+++ spn/include/core/Assert.h
@@ -1,6 +1,19 @@
 #ifndef CORE_ASSERT_H
 #define CORE_ASSERT_H
 
+#define CORE_TRAP				\
+	int* core_trap = 0;			\
+	*core_trap = 0;				\
+
+#define VALIDATE_AND_RETURN( x, ret )	\
+	if ( !( x ) )						\
+	{									\
+		CORE_TRAP;						\
+		return ret;						\
+	}
+
+#define VALIDATE( x ) VALIDATE_AND_RETURN( x, false )
+
 #define CHECK( x ) if ( !( x ) ) return false
 #define CHECK_AND_RETURN( x, ret ) if ( !( x ) ) return ret
 #define CHECK_S3E_RESULT( x ) if ( ( x ) != S3E_RESULT_SUCCESS ) return false
