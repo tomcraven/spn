0a7039a4628836639352be54645f66c9fd141c4b
Tom Craven <tcraven@amazon.com>
2015-02-04 18:06:59 +0000

    math: Math.h - add pi, pi2 and pi4 constants

---
+++ spn/include/math/Math.h
@@ -0,0 +1,13 @@
+#ifndef MATH_MATH_H
+#define MATH_MATH_H
+
+#include <cmath>
+
+namespace math
+{
+	const float pi = static_cast< float >( M_PI );
+	const float pi2 = static_cast< float >( M_PI * 2 );
+	const float pi4 = static_cast< float >( M_PI * 4 );
+}
+
+#endif
