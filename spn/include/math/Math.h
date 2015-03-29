df230ac55c55e9b2544ecf140e69812d1d4c1d26
Tom Craven <tcraven@amazon.com>
2015-03-29 12:12:12 +0100

    math: Math - remove dependency on M_PI

--- spn/include/math/Math.h
+++ spn/include/math/Math.h
@@ -5,9 +5,9 @@
 
 namespace math
 {
-	const float pi = static_cast< float >( M_PI );
-	const float pi2 = static_cast< float >( M_PI * 2 );
-	const float pi4 = static_cast< float >( M_PI * 4 );
+	const float pi = static_cast< float >( 3.14159265358979323846 );
+	const float pi2 = static_cast< float >( pi * 2 );
+	const float pi4 = static_cast< float >( pi * 4 );
 }
 
 #endif
