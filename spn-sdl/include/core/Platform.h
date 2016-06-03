9cc92a6df5211718c9a09dfa5ecab56914b773d3
Tom Craven <tom.cass.craven@gmail.com>
2016-06-03 07:08:22 +0100

    add linux version of sprintf

--- spn-sdl/include/core/Platform.h
+++ spn-sdl/include/core/Platform.h
@@ -5,6 +5,12 @@
 #include <SDL.h>
 #include <SDL_image.h>
 
+// This ifdef check is incorrect, we should be building platform specific versions of this platform file
+// Or is there an SDL version we could use? probably
+#ifdef WINDOWS
 #define SPRINTF( buffer, size, format, ... ) sprintf_s( buffer, size, format, ##__VA_ARGS__ )
+#else
+#define SPRINTF( buffer, size, format, ... ) sprintf( buffer, format, ##__VA_ARGS__ )
+#endif
 
 #endif
