4d5978bd494c68f2c432db46d11ce65d2f020f73
Tom Craven <tom.cass.craven@gmail.com>
2016-06-06 20:44:08 +0100

    core: assert - remove reference to S3E (marmalade specific)

--- spn/include/core/Assert.h
+++ spn/include/core/Assert.h
@@ -22,6 +22,5 @@
 
 #define CHECK( x ) if ( !( x ) ) return false
 #define CHECK_AND_RETURN( x, ret ) if ( !( x ) ) return ret
-#define CHECK_S3E_RESULT( x ) if ( ( x ) != S3E_RESULT_SUCCESS ) return false
 
 #endif
