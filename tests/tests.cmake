7840cf3ba6d88222ee44695d153fce187ec0aced
Tom Craven <tom.cass.craven@gmail.com>
2016-06-07 08:48:33 +0100

    tests: initial commit of cmake files and blank main test file

---
+++ tests/tests.cmake
@@ -0,0 +1,12 @@
+SET(tests_include ${CMAKE_CURRENT_LIST_DIR}/ext/googletest/googletest/include ${CMAKE_CURRENT_LIST_DIR}/ext/googletest/googletest)
+SET(tests_src
+	./ext/googletest/googletest/src/gtest.cc
+	./ext/googletest/googletest/src/gtest-death-test.cc
+	./ext/googletest/googletest/src/gtest-filepath.cc
+	./ext/googletest/googletest/src/gtest-port.cc
+	./ext/googletest/googletest/src/gtest-printers.cc
+	./ext/googletest/googletest/src/gtest-test-part.cc
+	./ext/googletest/googletest/src/gtest-typed-test.cc
+
+	./src/main.cpp
+)
